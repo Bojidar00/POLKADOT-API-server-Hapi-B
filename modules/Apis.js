4e0019601701b6759beb646fe19b6e5be3010633
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-10-20 11:52:21 +0300

    Small fix

--- modules/Apis.js
+++ modules/Apis.js
@@ -1,11 +1,13 @@
 
 const apiConnection = require('../nodeConnection')
+const dbConnection = require('../dbConnection')
 
 const connectApi = apiConnection.getNodeConnection().then((api) => {
     return api;
   });
-const connectApi = apiConnection.getNodeConnection().then((api) => {
-    return api;
+  const connectDb = dbConnection.getDbConnection().then((db) => {
+    db.connect().then(console.log("Connected to PostgreSQL from Server"));
+    return db;
 });
 exports.LastBlock= async (req, h) => {
            
