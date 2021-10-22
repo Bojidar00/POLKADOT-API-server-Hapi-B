2b9c2380a1361f33f546170436b01b33f20caf57
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-10-22 16:11:27 +0300

    Small fix

--- websockets.js
+++ websockets.js
@@ -1,7 +1,7 @@
 const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
-const apis = require('./modules/Apis');
+const apis = require('./modules/wsApis');
 
 
 
