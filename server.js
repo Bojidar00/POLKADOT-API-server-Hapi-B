1b0c172751b58d48b7c4ee79c40f87aaf7b7e835
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-10-24 14:17:22 +0300

    Small fix

--- server.js
+++ server.js
@@ -22,14 +22,25 @@ const init = async () => {
             return 'Hello World!';
         }
     });
-    server.route({
-        method: 'GET',
-        path: '/swagger',
-        handler: (request, h) => {
-            swaggerUi.setup(swaggerDocument);
-            return swaggerUi.serve();
+    await server.register([
+        require('inert'),
+        require('vision'),
+        {
+          plugin: require('hapi-swaggered-ui'),
+          options: {
+            title: 'Example API',
+            path: '/docs',
+            authorization: { // see above
+              field: 'apiKey',
+              scope: 'query', // header works as well
+              // valuePrefix: 'bearer '// prefix incase
+              defaultValue: 'demoKey',
+              placeholder: 'Enter your apiKey here'
+            },
+            swaggerOptions: {} // see above
+          }
         }
-    });
+      ])
     await server.register(require('./routes/routes'));
    
     await server.start();
